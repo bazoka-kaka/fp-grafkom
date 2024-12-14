0c02d06fbfbaac094fe41b6e0ec3d0b14bb84767
NadyaSaraswati <nsp.saraswati@gmail.com>
2024-12-15 04:09:38 +0700

    earth time

--- src/components/universe/sun/index.jsx
+++ src/components/universe/sun/index.jsx
@@ -1,7 +1,7 @@
 import { TextureLoader } from "three/src/loaders/TextureLoader";
-import { useLoader } from "@react-three/fiber";
+import { useLoader, useFrame } from "@react-three/fiber";
 import { EffectComposer, Bloom } from "@react-three/postprocessing";
-import { useRef } from "react";
+import { useRef, useEffect } from "react";
 import { Layers } from "three";
 
 const Sun = ({ position = [0, 0, -200], radius = 20 }) => {
@@ -13,22 +13,25 @@ const Sun = ({ position = [0, 0, -200], radius = 20 }) => {
   // Sun properties
   const widthSegments = 64;
   const heightSegments = 64;
-
-  // Assign the Sun to a specific bloom layer
   const BLOOM_LAYER = 1;
-  if (sunRef.current) {
-    sunRef.current.layers.enable(BLOOM_LAYER);
-  }
+
+  useEffect(() => {
+    if (sunRef.current) {
+      sunRef.current.layers.enable(BLOOM_LAYER);
+    }
+  }, [sunRef]);
+
+  // Add rotation to the Sun
+  useFrame(() => {
+    if (sunRef.current) {
+      sunRef.current.rotation.y += 0.005; // Rotate Sun on Y-axis
+    }
+  });
 
   return (
     <group>
       {/* Add point light to illuminate nearby objects like Earth */}
-      <pointLight
-        position={position}
-        intensity={5}
-        distance={500}
-        decay={2}
-      />
+      <pointLight position={position} intensity={5} distance={500} decay={2} />
 
       {/* Sun Mesh */}
       <mesh ref={sunRef} position={position}>
@@ -36,7 +39,7 @@ const Sun = ({ position = [0, 0, -200], radius = 20 }) => {
         <meshStandardMaterial
           map={sunMap} // Apply Sun texture
           emissive="#FFA500" // Glow effect color (orange)
-          emissiveIntensity={0.3} // Glow intensity
+          emissiveIntensity={0.5} // Glow intensity
         />
       </mesh>
     </group>
